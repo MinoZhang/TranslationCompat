224dde0dd879bdf6c60e6d23eddb4713acaf8e9c
MinoZhang <zhangboya@tarenwang.com>
2016-11-07 17:56:57 +0800

    完成登陆界面搭建

---
+++ app/src/main/java/com/learn/translationcompat/MainActivity.java
@@ -0,0 +1,32 @@
+package com.learn.translationcompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.learn.translationcompat.Login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.btn_login:
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                break;
+            case R.id.btn_list1:
+                break;
+            case R.id.btn_list2:
+                break;
+            case R.id.btn_list3:
+                break;
+        }
+    }
+}
